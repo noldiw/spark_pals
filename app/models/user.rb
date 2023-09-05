f6e786ad5483d7e4daa638bfc5c8d847dff49513
nolwenn diouf <nolwenn.df@gmail.com>
2023-09-05 16:22:05 +0200

    Created database tables

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Define additional attributes for the User model
+  attr_accessor :username, :first_name, :last_name
+end
---
+++ db/migrate/20230905133540_create.rb
@@ -0,0 +1,4 @@
+class Create < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
